6bf9ca8aa79ca246452da7b3c445e222da72c3ed
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-16 01:54:08 +0900

    point_count == 0 に対応
    バグっぽいのを回避

--- DSL_2_C.py
+++ DSL_2_C.py
@@ -5,6 +5,7 @@ from itertools import islice
 from math import sqrt
 from operator import itemgetter, methodcaller
 from sys import stdin
+from typing import Iterator
 
 consume = deque(maxlen=0).extend
 
@@ -16,22 +17,23 @@ points = [
             map(methodcaller('split'), islice(stdin, point_count))))]
 points.sort()
 
-x_node_interval = int(sqrt(point_count))
-lower_bounds = [p[0] for p in points[::x_node_interval]]
-upper_bounds = [p[0] for p in points[x_node_interval - 1::x_node_interval]]
-upper_bounds.append(1_000_000_000 + 1)
-x_ascending_groups = [
-    points[i:i + x_node_interval] for i in
-    range(0, point_count, x_node_interval)]
-consume(map(methodcaller('sort', key=itemgetter(1)), x_ascending_groups))
-x_ascending_groups = [
-    ([p[1] for p in group], group) for group in x_ascending_groups]
+if point_count > 0:
+    x_node_interval = int(sqrt(point_count))
+    lower_bounds = [p[0] for p in points[::x_node_interval]]
+    upper_bounds = [p[0] for p in points[x_node_interval - 1::x_node_interval]]
+    upper_bounds.append(1_000_000_000 + 1)
+    x_ascending_groups = [
+        points[i:i + x_node_interval] for i in
+        range(0, point_count, x_node_interval)]
+    consume(map(methodcaller('sort', key=itemgetter(1)), x_ascending_groups))
+    x_ascending_groups = [
+        ([p[1] for p in group], group) for group in x_ascending_groups]
 
 query_count = int(input())
-for sx, tx, sy, ty in map(
-    tuple,
-        map(partial(map, int),
-            map(methodcaller('split'), islice(stdin, query_count)))):
+
+
+def _solve_query(it: Iterator[int]) -> None:
+    sx, tx, sy, ty = (*it,)
     answer = []
     group_start = bisect_left(upper_bounds, sx)
     for group_index in range(
@@ -39,10 +41,16 @@ for sx, tx, sy, ty in map(
         ys, members = x_ascending_groups[group_index]
         member_start = bisect_left(ys, sy)
         for member_index in range(
-                member_start, bisect_right(ys, ty, lo=member_start)):
+                member_start, bisect_right(ys, ty)):  # bug? lo に何か（デフォルトの 0 でも）与えると #14 で無限ループを起こすっぽい
             if sx <= members[member_index][0] <= tx:
                 answer.append(members[member_index][2])
     if answer:
         answer.sort()
         print('\n'.join(map(str, answer)))
+
+
+solve_query = _solve_query if point_count > 0 else lambda _: None
+for it in map(partial(map, int),
+              map(methodcaller('split'), islice(stdin, query_count))):
+    solve_query(it)
     print()
