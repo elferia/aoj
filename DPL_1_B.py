c67cf0e8f0665e8de635add075fbb66eca29a1f2
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-20 22:25:03 +0900

    for文の完全排除
    00.53sec, 8040KB

--- DPL_1_B.py
+++ DPL_1_B.py
@@ -1,7 +1,9 @@
+from collections import deque
 from functools import partial
 from itertools import islice
 from operator import methodcaller
 from sys import stdin
+from typing import Tuple
 
 item_count, max_weight = [int(s) for s in input().split()]
 items = [
@@ -12,15 +14,18 @@ items = [
 values = [0]
 values.extend(-1 for _ in range(max_weight))
 
-for item_index in range(item_count):
-    item_weight = items[item_index][1]
+
+def update_values(item: Tuple[int, int]) -> None:
+    item_weight = item[1]
 
     def max_value(current_weight: int) -> int:
         light_value = values[current_weight - item_weight]
         return max(
-            -1 if light_value == -1 else light_value + items[item_index][0],
+            -1 if light_value == -1 else light_value + item[0],
             values[current_weight])
 
     values[item_weight:] = map(max_value, range(item_weight, max_weight + 1))
 
+
+deque(map(update_values, items), maxlen=0)
 print(max(values))
