9369c66e75a5914d6841644f92338838754f829a
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-20 14:46:12 +0900

    DP
    00.99sec, 7684KB

--- DPL_1_A.py
+++ DPL_1_A.py
@@ -1,20 +1,28 @@
 from array import array
 
 amount, coin_kind_count = [int(s) for s in input().split()]
-coin_kinds = array('H', (n for n in map(int, input().split()) if n <= amount))
+coin_kinds = array('H', [1])
+coin_kinds.extend(n for n in map(int, input().split()) if 1 < n <= amount)
+
+min_coin_count = [array('H', range(amount + 1))]
 
 
-def min_coin_count(coin_index_stop: int, current_amount: int) -> int:
-    if coin_index_stop == 0 or current_amount < 0:
-        return amount + 1
+def ith_amount_min_coin_count(i: int, current_amount: int) -> int:
+    ith_value = coin_kinds[i]
+    if ith_value == current_amount:
+        return 1
 
-    if current_amount == 0:
-        return 0
+    without_ith_count = min_coin_count[i - 1][current_amount]
+    if ith_value > current_amount:
+        return without_ith_count
 
     return min(
-        min_coin_count(
-            coin_index_stop, current_amount - coin_kinds[coin_index_stop - 1]
-        ) + 1, min_coin_count(coin_index_stop - 1, current_amount))
+        without_ith_count, min_coin_count[i][current_amount - ith_value] + 1)
 
 
-print(min_coin_count(len(coin_kinds), amount))
+for i in range(1, len(coin_kinds)):
+    min_coin_count.append(array('H', [0]))
+    for current_amount in range(1, amount + 1):
+        min_coin_count[-1].append(ith_amount_min_coin_count(i, current_amount))
+
+print(min_coin_count[len(coin_kinds) - 1][amount])
