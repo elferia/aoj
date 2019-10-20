4b6ecb9d3e62926fe495030782d9bb3d91854fc0
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-21 00:13:01 +0900

    for文の排除
    00.24sec, 7924KB

--- DPL_1_A.py
+++ DPL_1_A.py
@@ -1,16 +1,18 @@
+from collections import deque
+
 amount, coin_kind_count = [int(s) for s in input().split()]
-coin_kinds = [1]
-coin_kinds.extend(n for n in map(int, input().split()) if 1 < n <= amount)
+coin_kinds = (n for n in map(int, input().split()) if 1 < n <= amount)
 
 min_coin_count = list(range(amount + 1))
 
 
-for i in range(1, len(coin_kinds)):
-    ith_value = coin_kinds[i]
+def update_min_coin_count(ith_value: int) -> None:
     min_coin_count[ith_value] = 1
     for current_amount in range(ith_value + 1, amount + 1):
         use_ith_count = min_coin_count[current_amount - ith_value] + 1
         if use_ith_count < min_coin_count[current_amount]:
             min_coin_count[current_amount] = use_ith_count
 
+
+deque(map(update_min_coin_count, coin_kinds), maxlen=0)
 print(min_coin_count[-1])
