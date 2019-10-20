52a80ce61aa0cb6aa99e55e44b76076a17266c42
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-20 14:55:56 +0900

    1つ前しか要らない
    00.73sec, 9616KB

--- DPL_1_A.py
+++ DPL_1_A.py
@@ -2,7 +2,7 @@ amount, coin_kind_count = [int(s) for s in input().split()]
 coin_kinds = [1]
 coin_kinds.extend(n for n in map(int, input().split()) if 1 < n <= amount)
 
-min_coin_count = [list(range(amount + 1))]
+min_coin_count = list(range(amount + 1))
 
 
 def ith_amount_min_coin_count(i: int, current_amount: int) -> int:
@@ -10,17 +10,18 @@ def ith_amount_min_coin_count(i: int, current_amount: int) -> int:
     if ith_value == current_amount:
         return 1
 
-    without_ith_count = min_coin_count[i - 1][current_amount]
+    without_ith_count = min_coin_count[current_amount]
     if ith_value > current_amount:
         return without_ith_count
 
     return min(
-        without_ith_count, min_coin_count[i][current_amount - ith_value] + 1)
+        without_ith_count, _min_coin_count[current_amount - ith_value] + 1)
 
 
 for i in range(1, len(coin_kinds)):
-    min_coin_count.append([0])
+    _min_coin_count = [0]
     for current_amount in range(1, amount + 1):
-        min_coin_count[-1].append(ith_amount_min_coin_count(i, current_amount))
+        _min_coin_count.append(ith_amount_min_coin_count(i, current_amount))
+    min_coin_count = _min_coin_count
 
-print(min_coin_count[len(coin_kinds) - 1][amount])
+print(min_coin_count[amount])
