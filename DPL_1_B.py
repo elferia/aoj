e5d2293d87b7be330e77bb6733042470e905092a
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-20 22:19:15 +0900

    for文の排除、一時配列の削除
    00.57sec, 7020KB

--- DPL_1_B.py
+++ DPL_1_B.py
@@ -14,12 +14,13 @@ values.extend(-1 for _ in range(max_weight))
 
 for item_index in range(item_count):
     item_weight = items[item_index][1]
-    _values = values[:item_weight]
-    for current_weight in range(item_weight, max_weight + 1):
+
+    def max_value(current_weight: int) -> int:
         light_value = values[current_weight - item_weight]
-        _values.append(
-            max((-1 if light_value == -1 else
-                 light_value + items[item_index][0]), values[current_weight]))
-    values = _values
+        return max(
+            -1 if light_value == -1 else light_value + items[item_index][0],
+            values[current_weight])
+
+    values[item_weight:] = map(max_value, range(item_weight, max_weight + 1))
 
 print(max(values))
