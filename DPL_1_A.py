acb660bc007de788998b4efd6822eef89f565195
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-20 17:08:51 +0900

    究極まで簡素化した
    00.37sec, 7516KB

--- DPL_1_A.py
+++ DPL_1_A.py
@@ -5,19 +5,12 @@ coin_kinds.extend(n for n in map(int, input().split()) if 1 < n <= amount)
 min_coin_count = list(range(amount + 1))
 
 
-def ith_amount_min_coin_count(i: int, current_amount: int) -> int:
-    ith_value = coin_kinds[i]
-    if ith_value == current_amount:
-        return 1
-
-    without_ith_count = min_coin_count[current_amount]
-    return min(
-        without_ith_count, min_coin_count[current_amount - ith_value] + 1)
-
-
 for i in range(1, len(coin_kinds)):
-    for current_amount in range(coin_kinds[i], amount + 1):
-        min_coin_count[current_amount] = ith_amount_min_coin_count(
-            i, current_amount)
+    ith_value = coin_kinds[i]
+    min_coin_count[ith_value] = 1
+    for current_amount in range(ith_value + 1, amount + 1):
+        use_ith_count = min_coin_count[current_amount - ith_value] + 1
+        if use_ith_count < min_coin_count[current_amount]:
+            min_coin_count[current_amount] = use_ith_count
 
-print(min_coin_count[amount])
+print(min_coin_count[-1])
