0410588b5d17b5f1886af737737d09046826e21c
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-23 08:03:57 +0900

    データを再検討して処理の簡約
    - valueがすでに存在するとき、必ずincre_s_lenは等しい
    - incre_s_lensは1, 2, 3, 4, ...なので、直後の要素を乗っ取れば良い
    
    #21 00.03s
    #25 00.12s
    #26 00.14s
    #28 00.14s
    #29 00.19s

--- DPL_1_D.py
+++ DPL_1_D.py
@@ -11,27 +11,13 @@ incre_s_lens = []
 
 def update(value: int) -> None:
     pos = bisect_left(values, value)
-    incre_s_len = (incre_s_lens[pos - 1] if pos else 0) + 1
-
-    def remove_elem(i: int) -> None:
-        if incre_s_lens[i] == incre_s_len:
-            values.pop(i)
-            incre_s_lens.pop(i)
 
     if pos == len(values):
+        incre_s_len = (incre_s_lens[pos - 1] if pos else 0) + 1
         values.append(value)
         incre_s_lens.append(incre_s_len)
-    elif value == values[pos]:
-        if incre_s_len > incre_s_lens[pos]:
-            incre_s_lens[pos] = incre_s_len
-            for i in range(len(incre_s_lens) - 1, pos, -1):
-                remove_elem(i)
-    else:
-        for i in range(len(incre_s_lens) - 1, pos - 1, -1):
-            remove_elem(i)
-
-        values.insert(pos, value)
-        incre_s_lens.insert(pos, incre_s_len)
+    elif value < values[pos]:
+        values[pos] = value
 
 
 for i in array:
