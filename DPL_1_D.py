79e5036dd6e649d008b64252ee13ee1820b3ca10
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-22 12:47:29 +0900

    とりあえず解き方は間違ってない
    #26 TLE
    #21 00.75s
    #25 00.37s

---
+++ DPL_1_D.py
@@ -0,0 +1,26 @@
+from bisect import bisect_left
+from itertools import islice
+from sys import stdin
+
+array_len = int(input())
+array = map(int, islice(stdin, array_len))
+
+values = []
+incre_s_lens = []
+
+
+def update(value: int) -> None:
+    pos = bisect_left(values, value)
+    incre_s_len = max(incre_s_lens[:pos], default=0) + 1
+    if pos < len(values) and value == values[pos]:
+        if incre_s_len > incre_s_lens[pos]:
+            incre_s_lens[pos] = incre_s_len
+    else:
+        values.insert(pos, value)
+        incre_s_lens.insert(pos, incre_s_len)
+
+
+for i in array:
+    update(i)
+
+print(max(incre_s_lens))
