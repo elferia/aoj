845d35105fb60e6201517489b80f54c19815f965
elferia <a.t.qu.qu.qu@gmail.com>
2019-10-22 23:01:37 +0900

    唯一のlongest ILになる見込みのない点を削除する
    #29 TLE
    #21 00.69s
    #25 00.42s
    #26 03.15s
    #28 03.21s

--- DPL_1_D.py
+++ DPL_1_D.py
@@ -12,10 +12,21 @@ incre_s_lens = []
 def update(value: int) -> None:
     pos = bisect_left(values, value)
     incre_s_len = max(islice(incre_s_lens, pos), default=0) + 1
+
+    def remove_elem(i: int) -> None:
+        if incre_s_lens[i] == incre_s_len:
+            values.pop(i)
+            incre_s_lens.pop(i)
+
     if pos < len(values) and value == values[pos]:
         if incre_s_len > incre_s_lens[pos]:
             incre_s_lens[pos] = incre_s_len
+            for i in range(len(incre_s_lens) - 1, pos, -1):
+                remove_elem(i)
     else:
+        for i in range(len(incre_s_lens) - 1, pos - 1, -1):
+            remove_elem(i)
+
         values.insert(pos, value)
         incre_s_lens.insert(pos, incre_s_len)
 
